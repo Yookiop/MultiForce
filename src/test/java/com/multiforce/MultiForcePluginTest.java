8735605a7bdc8f5bd56905269a6ea623a4f4c529
Yook <yookiop@hotmail.com>
2026-06-13 22:42:58 +0200

    test

--- src/test/java/com/multiforce/MultiForcePluginTest.java
+++ src/test/java/com/multiforce/MultiForcePluginTest.java
@@ -3,7 +3,7 @@ package com.multiforce;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MultiForcePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
